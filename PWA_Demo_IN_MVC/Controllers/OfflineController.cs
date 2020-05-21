5c32ee7462a02e1961ffb71c312e0eae011005fe
chandradev <Chandradev_Prasad@Dellteam.com>
2020-05-21 11:55:01 +0530

    Add project files.

---
+++ PWA_Demo_IN_MVC/Controllers/OfflineController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PWA_Demo_IN_MVC.Controllers
+{
+    public class OfflineController : Controller
+    {
+        // GET: Offline
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PWA_Demo_IN_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PWA_Demo_IN_MVC.MvcApplication" Language="C#" %>
